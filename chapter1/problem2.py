725da91e8619cefe27b636ab2f3cdf330a3b791b
VogueHQ <epicperson5533@gmail.com>
2026-05-31 00:11:00 +0530

    Create problem2.py

---
+++ chapter1/problem2.py
@@ -0,0 +1,4 @@
+"""just open terminal and then write python3 and then perform the table of 5 by writing 5*1
+like this you can perform the table of 5 up to 5*10
+this is called REPL(Read Evaluate Print Loop) in python"""
+print("Problem 2 solved successfully")
