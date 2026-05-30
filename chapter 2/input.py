b275a7b0d1ff9cf7f92cf920a542664a2de1a71b
VogueHQ <epicperson5533@gmail.com>
2026-05-31 00:20:23 +0530

    Create input.py

---
+++ chapter 2/input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter Number 1 : "))       #imput string hase to tene int ma change karvanu
+
+b = int(input("Enter Number 2 : "))
+
+print("Number a is ", a)
+print("Number b is ", b)    
+print("Sum of a and b is ", a+b) 
