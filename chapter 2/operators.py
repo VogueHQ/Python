e74fddb79175d14be264d2ebaf773fe3c798fe0c
VogueHQ <epicperson5533@gmail.com>
2026-05-31 00:18:54 +0530

    Create operators.py

---
+++ chapter 2/operators.py
@@ -0,0 +1,48 @@
+# arithmatic operators
+a = 7
+b = 4
+c = a + b # addition
+c = a - b # subtraction
+c = a * b # multiplication
+c = a / b # division
+c = a % b # modulus
+c = a ** b # exponentiation
+c = a // b # floor division
+
+# comparison operators
+c = a == b # equal to
+c = a != b # not equal to
+c = a > b # greater than
+c = a < b # less than
+c = a >= b # greater than or equal to
+c = a <= b # less than or equal to  
+comparison_result = a > b # comparison operators return boolean values
+
+
+# assignment operators
+a = 4-2
+b = 6
+b += a # b = b + a
+b -= a # b = b - a
+
+# logical operators
+
+e = True or False
+
+# Truth table for 'or' operator:
+print("True or False is ", True or False)   # Output: True
+print("True or True is ", True or True)     # Output: True
+print("False or False is ", False or False) # Output: False
+print("False or True is ", False or True)   # Output: True
+
+# Truth table for 'and' operator:
+print("True and False is ", True and False)   # Output: False
+print("True and True is ", True and True)     # Output: True
+print("False and False is ", False and False) # Output: False
+print("False and True is ", False and True)   # Output: False
+
+# Truth table for 'not' operator:
+print("not True is ", not True)   # Output: False
+print("not False is ", not False) # Output: True    
+
+print(not(True))
