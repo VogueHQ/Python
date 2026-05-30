e7d347a45278a28c813db568daca0fec07d8c17b
VogueHQ <epicperson5533@gmail.com>
2026-05-30 23:59:49 +0530

    Added first Python file

---
+++ First.py
@@ -0,0 +1,5 @@
+print("Hello World")
+#  if you want to use comments then use ctrl + / for single line comments
+"""
+if you want to use multi line comments then use ctrl + shift + / for multi line comments or you can use triple quotes for multi line comments
+"""
