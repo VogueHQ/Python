b2595a31c15d5e2755d334cc0c2a48254210a3f2
VogueHQ <epicperson5533@gmail.com>
2026-05-31 00:23:54 +0530

    Create problelm6.py

---
+++ chapter 2/problems/problelm6.py
@@ -0,0 +1,16 @@
+# Write a python program to calculate the square of a number entered by the user. 
+
+a = int(input("Enter Number 1:"))
+print("The square of the number is :", a*a)
+
+
+# or 
+
+# a = int(input("Enter Number 1:"))
+# b = a*a
+# print("The square of the number is :", b)
+
+
+
+# a**2 means power of a is 2
+# a**3 means power of a is 3
