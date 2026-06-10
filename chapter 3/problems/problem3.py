4b62fb2e077b4957da942efc299714c6b26c18f3
VogueHQ <epicperson5533@gmail.com>
2026-06-10 18:16:38 +0530

    Add double space detection in problem3.py
    Implement a program to detect double spaces in a string.

---
+++ chapter 3/problems/problem3.py
@@ -0,0 +1,7 @@
+# Write a program to detect double space in a string.
+
+name = "this is a good  boy and  "
+
+print(name.find("  "))
+
+# if write(sub string) something that is not in string then it will return value = -1
