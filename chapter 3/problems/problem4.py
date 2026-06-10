0aaa54ba660260d9c9ffaecffb73eb42ba7411a4
VogueHQ <epicperson5533@gmail.com>
2026-06-10 18:17:17 +0530

    Add script to replace double spaces in a string
    Add a script to replace double spaces with single spaces in a string.

---
+++ chapter 3/problems/problem4.py
@@ -0,0 +1,15 @@
+# Replace the double space from problem 3 with single spaces.
+
+name = "this is a good  boy and  "
+
+print(name.replace("  "," "))
+#👆🏻in this , name string does not change , it just made a new string and print that 
+
+
+# if i print original name string then it will print original name string (with that double space)
+
+
+print(name) 
+
+# Strings are immutable which means that you cannot change them by running functions on them
+
