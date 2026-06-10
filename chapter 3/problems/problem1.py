386c03bfa2b93450607bbd35923a3e4f1167155c
VogueHQ <epicperson5533@gmail.com>
2026-06-10 18:15:35 +0530

    Implement f-string for greeting in problem1.py
    Updated the greeting method to use f-string formatting.

---
+++ chapter 3/problems/problem1.py
@@ -0,0 +1,9 @@
+''' Write a python program to display a user entered name followed by Good
+ Afternoon using input () function.'''
+
+
+name = input("Enter Your Name : ")
+
+# print("Good Afternoon," , name)    this is old and traditional method but...
+
+print(f"Good Afternoon, {name}")      #this is new method and it is comulsory to difine f 
