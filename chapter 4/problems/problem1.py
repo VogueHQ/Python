ef62ae14efe0bc638e389a9c3adb9163feb2f685
VogueHQ <epicperson5533@gmail.com>
2026-06-15 18:33:46 +0530

    Add program to store user-entered favorite fruits

---
+++ chapter 4/problems/problem1.py
@@ -0,0 +1,27 @@
+# write a program to store seven fruits in a list entered by a user
+
+print("ENTER YOUR FAVOURITE FRUITS\n")
+
+Fruits = []
+
+f1 = input("ENTER FRUIT 1: ")
+Fruits.append(f1)
+f2 = input("ENTER FRUIT 2: ")
+Fruits.append(f2)
+f3 = input("ENTER FRUIT 3: ")
+Fruits.append(f3)
+f4 = input("ENTER FRUIT 4: ")
+Fruits.append(f4)
+f5 = input("ENTER FRUIT 5: ")
+Fruits.append(f5)
+f6 = input("ENTER FRUIT 6: ")
+Fruits.append(f6)
+f7 = input("ENTER FRUIT 7: ")
+Fruits.append(f7)
+
+print("\nHERE IS THE LIST OF YOUR ALL FAVOURITE FRUITS\n")
+
+print(Fruits) 
+
+print("\nThanks..!!!")
+
