c3abdc325499e9ca95222638a2f3b72863719e20
VogueHQ <epicperson5533@gmail.com>
2026-06-16 21:35:11 +0530

    Create Hindi to English dictionary program
    The program allows users to look up Hindi words and their English translations.

---
+++ chapter 5/problem/problem1.py
@@ -0,0 +1,12 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up! 
+
+words = {
+    "bhed" : "sheep",
+    "billi" : "cat" ,
+    "madad" : "help" 
+
+}
+
+word = input("Enter the word you want to translet : ")
+
+print(words[word])
