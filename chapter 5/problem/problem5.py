074627c937468894b678e59aa9a91cf67c65355f
VogueHQ <epicperson5533@gmail.com>
2026-06-16 23:11:07 +0530

    Create problem5.py to show dictionary type
    Add a Python script to demonstrate dictionary type.

---
+++ chapter 5/problem/problem5.py
@@ -0,0 +1,10 @@
+# s = {}
+# What is the type of 's'?
+
+
+'''type of s is dictionary 
+if we only write {} so this will be a dictionary 
+and if set{} then it will be empty set '''
+
+s = {}
+print(type(s))
