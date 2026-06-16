07b477cd83444f9eb4bf2166b77554e081ee311f
VogueHQ <epicperson5533@gmail.com>
2026-06-16 23:12:10 +0530

    Document behavior for same names in problem 6
    Add explanation for handling duplicate names in problem 6.

---
+++ chapter 5/problem/problem7.py
@@ -0,0 +1,8 @@
+'''Question : If the names of 2 friends are same; what will happen to the program in problem 6'''
+
+
+
+
+# Answer
+
+'''if we put name rohan 2 times then when we give value to that 2nd rohan name than it will update the value for second rohan because it is using update function so theat it will update (change) the value to the latest''' 
