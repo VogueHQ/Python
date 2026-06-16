3035dbff4f69d23dee30c275a3bbd282e8de7682
VogueHQ <epicperson5533@gmail.com>
2026-06-16 23:12:34 +0530

    Add comments on language similarity for problem 8
    Add explanation regarding language compatibility in problem 6.

---
+++ chapter 5/problem/problem8.py
@@ -0,0 +1,8 @@
+'''Question : If languages of two friends are same; what will happen to the program in problem
+6?'''
+
+
+# It's ok for having same value 
+# it will work asusual is we have same value 
+'''it means value can be same 
+but key cannot same because key will update everytime '''
