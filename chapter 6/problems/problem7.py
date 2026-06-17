02a1ee6fa4ba7c84ea66f0872e53c6990fd59610
VogueHQ <epicperson5533@gmail.com>
2026-06-17 14:22:08 +0530

    Add post analysis program for mentioning 'Stacy'
    The program checks if the input post mentions 'Stacy' and responds accordingly.

---
+++ chapter 6/problems/problem7.py
@@ -0,0 +1,16 @@
+# Write a program to find out whether a given post is talking about "Harry" or not.
+
+post = input("Enter your post: ")
+
+
+if("Stacy".lower() in post.lower()):
+    print("This post is talking about Stacy")
+
+else:
+    print("This post is talking about Stacy")
+
+
+
+# here .lower is used cuz if we write the name in small or capital letter then it will ditect using this .lower
+
+# .lower will convert whole post in lower and then convert name in lower then find name in post 
