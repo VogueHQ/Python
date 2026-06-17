9cb34e2af3213def1515cff5fcbb6446f6c8d4e0
VogueHQ <epicperson5533@gmail.com>
2026-06-17 14:21:05 +0530

    Add name presence check program
    The program checks if a user-provided name is in a predefined list.

---
+++ chapter 6/problems/problem5.py
@@ -0,0 +1,11 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+l = ["Stacy", "Ethan", "Victoria", "Charles", "Emma"]
+
+Name = input("Enter your name: ")
+
+if(Name in l):
+    print("Your name is in list")
+
+else:
+    print("Your name is not in list")
